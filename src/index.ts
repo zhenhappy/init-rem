e0800675ef7fb4b722b29cdb6ec980c2eda910fb
zhenhappy <q505507538@gmail.com>
2023-03-27 14:55:08 +0800

    重构成tsup打包

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-export const initRem = (options) => {
+export interface IOptions {
+  /** 定义设计图尺寸宽度 */
+  designWidth?: number;
+  /** 定义最大屏幕尺寸, 为了解决平板字体太大问题 */
+  maxWidth?: number;
+  /** 定义最大屏幕尺寸, 为了解决平板字体太大问题 */
+  minWidth?: number;
+  /** 定义文档默认字体大小 */
+  bodyFontSize?: number;
+  /**
+   * 定义基准字号
+   * 设置200便于计算, 在开发环境中Chrome用的是1倍图
+   * 那100px=1rem, 34px=0.34rem, 所有尺寸的实际尺寸和rem相差100倍
+   * 也不要小于12px, 虽然用10px也不错, 但是开发环境由于是Chrome,
+   * 小于12像素会有问题, 但是真实设备上是没问题的, 所以根据自己的开发环境决定
+   * */
+  baseFontSize?: number;
+}
+
+export const initRem = (options: IOptions) => {
   options = {
     designWidth: 750,  // 定义设计图尺寸宽度
     maxWidth: 500,     // 定义最大屏幕尺寸, 为了解决平板字体太大问题
@@ -17,7 +36,7 @@ export const initRem = (options) => {
   _setReCalcRem()
 
   // 设置文档默认字体
-  function _setBodyFontSize (loaded) {
+  function _setBodyFontSize (loaded?: boolean) {
     const head = document.head || document.getElementsByTagName('head')[0]
     const innerHTML = loaded
       ? `body { font-size: ${options.bodyFontSize}px; }`
